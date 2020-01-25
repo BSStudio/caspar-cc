0bfda97d69abc6bf0e27761df330858e2fd3ffe3
Komlós Boldizsár <komlos.boldizsar@gmail.com>
2020-01-25 18:46:26 +0100

    Add 3 main panels on GUI

---
+++ caspar-cc/MainForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace caspar_cc
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.actionsLabelPanel = new System.Windows.Forms.Panel();
+            this.actionsLabel = new System.Windows.Forms.Label();
+            this.actionsPanel = new System.Windows.Forms.Panel();
+            this.captionsLabelPanel = new System.Windows.Forms.Panel();
+            this.captionsLabel = new System.Windows.Forms.Label();
+            this.captionsPanel = new System.Windows.Forms.Panel();
+            this.channelsLabelPanel = new System.Windows.Forms.Panel();
+            this.channelsLabel = new System.Windows.Forms.Label();
+            this.channelsPanel = new System.Windows.Forms.Panel();
+            this.actionsLabelPanel.SuspendLayout();
+            this.captionsLabelPanel.SuspendLayout();
+            this.channelsLabelPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // actionsLabelPanel
+            // 
+            this.actionsLabelPanel.AutoSize = true;
+            this.actionsLabelPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.actionsLabelPanel.BackColor = System.Drawing.Color.Navy;
+            this.actionsLabelPanel.Controls.Add(this.actionsLabel);
+            this.actionsLabelPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.actionsLabelPanel.Location = new System.Drawing.Point(0, 0);
+            this.actionsLabelPanel.Name = "actionsLabelPanel";
+            this.actionsLabelPanel.Size = new System.Drawing.Size(800, 58);
+            this.actionsLabelPanel.TabIndex = 0;
+            // 
+            // actionsLabel
+            // 
+            this.actionsLabel.AutoSize = true;
+            this.actionsLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.actionsLabel.Font = new System.Drawing.Font("Broadway", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.actionsLabel.ForeColor = System.Drawing.Color.White;
+            this.actionsLabel.Location = new System.Drawing.Point(0, 0);
+            this.actionsLabel.Name = "actionsLabel";
+            this.actionsLabel.Padding = new System.Windows.Forms.Padding(10);
+            this.actionsLabel.Size = new System.Drawing.Size(196, 58);
+            this.actionsLabel.TabIndex = 0;
+            this.actionsLabel.Text = "ACTIONS";
+            // 
+            // actionsPanel
+            // 
+            this.actionsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.actionsPanel.Location = new System.Drawing.Point(0, 58);
+            this.actionsPanel.Name = "actionsPanel";
+            this.actionsPanel.Padding = new System.Windows.Forms.Padding(10);
+            this.actionsPanel.Size = new System.Drawing.Size(800, 100);
+            this.actionsPanel.TabIndex = 1;
+            // 
+            // captionsLabelPanel
+            // 
+            this.captionsLabelPanel.AutoSize = true;
+            this.captionsLabelPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.captionsLabelPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.captionsLabelPanel.Controls.Add(this.captionsLabel);
+            this.captionsLabelPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.captionsLabelPanel.Location = new System.Drawing.Point(0, 158);
+            this.captionsLabelPanel.Name = "captionsLabelPanel";
+            this.captionsLabelPanel.Size = new System.Drawing.Size(800, 58);
+            this.captionsLabelPanel.TabIndex = 2;
+            // 
+            // captionsLabel
+            // 
+            this.captionsLabel.AutoSize = true;
+            this.captionsLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.captionsLabel.Font = new System.Drawing.Font("Broadway", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.captionsLabel.ForeColor = System.Drawing.Color.White;
+            this.captionsLabel.Location = new System.Drawing.Point(0, 0);
+            this.captionsLabel.Name = "captionsLabel";
+            this.captionsLabel.Padding = new System.Windows.Forms.Padding(10);
+            this.captionsLabel.Size = new System.Drawing.Size(230, 58);
+            this.captionsLabel.TabIndex = 0;
+            this.captionsLabel.Text = "CAPTIONS";
+            // 
+            // captionsPanel
+            // 
+            this.captionsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.captionsPanel.Location = new System.Drawing.Point(0, 216);
+            this.captionsPanel.Name = "captionsPanel";
+            this.captionsPanel.Size = new System.Drawing.Size(800, 150);
+            this.captionsPanel.TabIndex = 3;
+            // 
+            // channelsLabelPanel
+            // 
+            this.channelsLabelPanel.AutoSize = true;
+            this.channelsLabelPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.channelsLabelPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.channelsLabelPanel.Controls.Add(this.channelsLabel);
+            this.channelsLabelPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.channelsLabelPanel.Location = new System.Drawing.Point(0, 366);
+            this.channelsLabelPanel.Name = "channelsLabelPanel";
+            this.channelsLabelPanel.Size = new System.Drawing.Size(800, 58);
+            this.channelsLabelPanel.TabIndex = 4;
+            // 
+            // channelsLabel
+            // 
+            this.channelsLabel.AutoSize = true;
+            this.channelsLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.channelsLabel.Font = new System.Drawing.Font("Broadway", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.channelsLabel.ForeColor = System.Drawing.Color.White;
+            this.channelsLabel.Location = new System.Drawing.Point(0, 0);
+            this.channelsLabel.Name = "channelsLabel";
+            this.channelsLabel.Padding = new System.Windows.Forms.Padding(10);
+            this.channelsLabel.Size = new System.Drawing.Size(238, 58);
+            this.channelsLabel.TabIndex = 0;
+            this.channelsLabel.Text = "CHANNELS";
+            // 
+            // channelsPanel
+            // 
+            this.channelsPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.channelsPanel.Location = new System.Drawing.Point(0, 424);
+            this.channelsPanel.Name = "channelsPanel";
+            this.channelsPanel.Size = new System.Drawing.Size(800, 100);
+            this.channelsPanel.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.captionsPanel);
+            this.Controls.Add(this.channelsLabelPanel);
+            this.Controls.Add(this.channelsPanel);
+            this.Controls.Add(this.captionsLabelPanel);
+            this.Controls.Add(this.actionsPanel);
+            this.Controls.Add(this.actionsLabelPanel);
+            this.Name = "MainForm";
+            this.Text = "CasparCG - closed captions GUI";
+            this.actionsLabelPanel.ResumeLayout(false);
+            this.actionsLabelPanel.PerformLayout();
+            this.captionsLabelPanel.ResumeLayout(false);
+            this.captionsLabelPanel.PerformLayout();
+            this.channelsLabelPanel.ResumeLayout(false);
+            this.channelsLabelPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel actionsLabelPanel;
+        private System.Windows.Forms.Label actionsLabel;
+        private System.Windows.Forms.Panel actionsPanel;
+        private System.Windows.Forms.Panel captionsLabelPanel;
+        private System.Windows.Forms.Label captionsLabel;
+        private System.Windows.Forms.Panel captionsPanel;
+        private System.Windows.Forms.Panel channelsLabelPanel;
+        private System.Windows.Forms.Label channelsLabel;
+        private System.Windows.Forms.Panel channelsPanel;
+    }
+}
+
